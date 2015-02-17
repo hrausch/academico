5b70ba5dae839d072ce8accfcd89d7965fa90296
herbertrf <hrausch@gmail.com>
2015-02-17 17:23:00 -0200

    Adicionando o primeiro código JAVA..

---
+++ java/conceitos-iniciais/ProgramaJava1.java
@@ -0,0 +1,9 @@
+public class ProgramaJava1{
+
+	public static void main(String [] args){
+	
+		/* Imprime no console a frase: 'Meu primeiro programa java' */
+		System.out.println("Meu primeiro programa java");
+	
+	}
+}
