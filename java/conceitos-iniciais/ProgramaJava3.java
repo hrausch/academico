814ce2e67be82163efccd1662a2d56224a697ab1
hrausch <hrausch@gmail.com>
2015-02-18 20:31:32 -0200

    Create ProgramaJava3.java
    Adicionando o terceiro programa com os varios tipos primitivos da linguagem e estruturas básicas

---
+++ java/conceitos-iniciais/ProgramaJava3.java
@@ -0,0 +1,16 @@
+public class ProgramaJava3{
+
+	public static void main(String [] args){
+	
+		String parametro = args[0];
+		String texto = "O parâmetro recebido foi: " + parametro;
+		
+		int a;
+		a = 20;
+		int b = 12;
+		int c = ( a * b )/3 
+	
+		System.out.println("O valor digitado foi: " + parametro);
+	
+	}
+}
