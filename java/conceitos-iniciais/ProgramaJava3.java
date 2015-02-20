28b4617438ac814d8cefa188233cd1f5a0c1f140
herbertrf <hrausch@gmail.com>
2015-02-20 20:26:15 -0200

    Adiconando novos exemplos de código fonte.

--- java/conceitos-iniciais/ProgramaJava3.java
+++ java/conceitos-iniciais/ProgramaJava3.java
@@ -8,9 +8,26 @@ public class ProgramaJava3{
 		int a;
 		a = 20;
 		int b = 12;
-		int c = ( a * b )/3 
+		int c = ( a * b ) / 3;
+		int d = a / b / c;		
 	
 		System.out.println("O valor digitado foi: " + parametro);
+		
+		System.out.println("O resultado do calculo 1 foi: " + c);
+		
+		System.out.println("O resultado do calculo 2 foi: " + d);
+		
+		if( c == 0){
+			System.out.println("C eh igual a zero");
+		}
+		
+		if( c > 0){
+			System.out.println("C eh maior que zero");		
+		}
+		
+		if( c < 0){
+			System.out.println("C eh menor que zero");
+		}
 	
 	}
 }
