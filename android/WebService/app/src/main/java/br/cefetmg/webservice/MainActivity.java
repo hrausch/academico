e6f8400f4a55e4fa498256dccc424f6d299b74a8
Herbert rausch <hrausch@gmail.com>
2016-09-19 21:03:38 -0300

    added web service sample

---
+++ android/WebService/app/src/main/java/br/cefetmg/webservice/MainActivity.java
@@ -0,0 +1,112 @@
+package br.cefetmg.webservice;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import br.cefetmg.webservice.model.Endereco;
+import br.cefetmg.webservice.restclient.RestFulClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    EditText editRua, editCidade, editEstado;
+    Endereco objEndereco;
+    Button btnSalvar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exemplo4_list);
+
+        btnSalvar = (Button) findViewById(R.id.button);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        ArrayList<Endereco> list = new ArrayList<Endereco>();
+    }
+
+
+    public void salvarEBuscarEndereco(View v){
+
+        btnSalvar.setEnabled(false);
+
+        objEndereco = new Endereco();
+
+        editRua = (EditText) findViewById(R.id.editRua);
+        editCidade = (EditText) findViewById(R.id.editCidade);
+        editEstado = (EditText) findViewById(R.id.editUF);
+
+
+
+        objEndereco.setRua(editRua.getText().toString());
+        objEndereco.setCidade(editCidade.getText().toString());
+        objEndereco.setEstado(editEstado.getText().toString());
+        Log.e("APP", "Chamou");
+        AsyncTask taskWs = new ConnectionEndereco();
+        taskWs.execute();
+
+    }
+
+
+
+    /***
+     * Implementacao da classe que ira roda em umma Thread separa da Thread da Interface
+     */
+    class ConnectionEndereco extends AsyncTask<Object, Void,  ArrayList<Endereco>> {
+
+
+        private RestFulClient cliente = new RestFulClient();
+
+
+        /** Este metodo eh executado por um Thread que eh controlado pela classe
+         *  Quando esta Thread finaliza a execucao, automaticamente o metodo
+         *  onPostExecute eh executado
+         */
+        @Override
+        protected  ArrayList<Endereco> doInBackground(Object ... args) {
+
+
+            //salva e em seguida lista os dados registrados
+
+            ArrayList<Endereco> list = new ArrayList<Endereco>();
+
+            cliente.salvarEndereco(objEndereco);
+
+            String jsonEnderecos = cliente.recuperarEnderecos();
+            list = Endereco.fromArrayJson(jsonEnderecos);
+
+            return list;
+
+        }
+
+        /** Este metodo eh automaticamente executado quando doInBackground() finalizar
+         * Neste momento ele irah renderizar o listview com os dados recuperados no webservice
+         */
+        protected void onPostExecute(ArrayList<Endereco> result) {
+            Context context = getApplicationContext();
+            CharSequence text = "Rua Salva com Sucesso!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            btnSalvar.setEnabled(true);
+
+
+//            eAdapter.setLista(result);
+//            listView.setAdapter(eAdapter);
+
+        }
+    }
+}
---
+++ android/WebService/app/src/main/java/br/cefetmg/webservice/model/Endereco.java
@@ -0,0 +1,85 @@
+package br.cefetmg.webservice.model;
+
+/**
+ * Created by hrausch on 17/06/16.
+ */
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Endereco implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+
+
+    private String rua;
+    private String estado;
+    private String cidade;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+
+    /**
+     * Recebe um json que representa um objeto e o transforma em Object
+     * @param objectJson
+     * @return
+     */
+    public static Endereco fromJson(String objectJson) {
+        Gson gson = new Gson();
+        Endereco e = gson.fromJson(objectJson, Endereco.class);
+        return e;
+    }
+
+    /**
+     * Recebe um json que representa uma lista de objetos e retorna em ArrayList<>
+     * @param objectJson
+     * @return
+     */
+    public static ArrayList<Endereco> fromArrayJson(String objectJson) {
+        Gson gson = new Gson();
+        ArrayList<Endereco> e = new ArrayList<Endereco>();
+
+        JsonParser parser = new JsonParser();
+        JsonArray array = parser.parse(objectJson).getAsJsonArray();
+
+        int tamanho = array.size();
+        for (int i = 0; i < tamanho; i++) {
+
+            Endereco endereco = gson.fromJson(array.get(i), Endereco.class);
+            e.add(endereco);
+        }
+
+        return e;
+    }
+
+}
